3384f29df01da82655de5012f13ddf11740d170a
formal-acc <sarthak.agrawal@research.iiit.ac.in>
2020-10-05 14:29:34 +0530

    Implemented pep8 conventions

--- Phase4/ui.py
+++ Phase4/ui.py
@@ -2,89 +2,95 @@ import subprocess as sp
 import pymysql
 import pymysql.err
 import pymysql.cursors
-import re
-from datetime import datetime
 
 """
 Functions for different options
 """
-def getFeedback(): # Finds specific record in Feedback 
+
+
+def get_feedback():  # Finds specific record in Feedback
     try:
         entry = {}
         print("Enter the Feedback details:- ")
-        entry['Waiter'] = input("Waiter ID: ").strip()
-        entry['Chef'] = input("Chef ID: ").strip()
-        entry['Dish'] = input("Dish name: ").strip()
-        entry['Phone'] = input("Phone: ").strip()
-        entry['Time'] = input("Time as YYYY-MM-DD HH:MM:SS ").strip()
-        
-        cur.execute("SELECT * FROM Feedback WHERE Waiter_id=%s AND Chef_id=%s AND Dish_name=%s AND Phone=%s AND Time=%s", (entry['Waiter'], entry['Chef'], entry['Dish'], entry['Phone'], entry['Time']))
+        entry["Waiter"] = input("Waiter ID: ").strip()
+        entry["Chef"] = input("Chef ID: ").strip()
+        entry["Dish"] = input("Dish name: ").strip()
+        entry["Phone"] = input("Phone: ").strip()
+        entry["Time"] = input("Time as YYYY-MM-DD HH:MM:SS ").strip()
+
+        cur.execute(
+            "SELECT * FROM Feedback WHERE Waiter_id=%s AND Chef_id=%s AND Dish_name=%s AND Phone=%s AND Time=%s",
+            (
+                entry["Waiter"],
+                entry["Chef"],
+                entry["Dish"],
+                entry["Phone"],
+                entry["Time"],
+            ),
+        )
         rows = cur.fetchall()
         for row in rows:
             print(row)
         con.commit()
         print()
-    
+
     except MySQLError as e:
         con.rollback()
-        print('Encountered Database error {!r}, Error number- {}'.format(e, e.args[0]))
-
-    except Exception as e:
-        con.rollback()
-        print("Failed")
-        print(">>>>>>>>>>>>>", e)
+        print("Encountered Database error {!r}, Error number- {}".format(e, e.args[0]))
+        print("-" * 10)
 
     return
 
-def employeeFeedback(): # Finds all records in Feedback for given employee 
+
+def employee_feedback():  # Finds all records in Feedback for given employee
     try:
         employee = input("Enter EmployeeID: ").strip()
 
-        cur.execute("SELECT * FROM Feedback WHERE Waiter_id=%s OR Chef_id=%s", (employee, employee))        
+        cur.execute(
+            "SELECT * FROM Feedback WHERE Waiter_id=%s OR Chef_id=%s",
+            (employee, employee),
+        )
         rows = cur.fetchall()
         for row in rows:
             print(row)
         con.commit()
         print()
-    
+
     except MySQLError as e:
         con.rollback()
-        print('Encountered Database error {!r}, Error number- {}'.format(e, e.args[0]))
-
-    except Exception as e:
-        con.rollback()
-        print("Failed")
-        print(">>>>>>>>>>>>>", e)
+        print("Encountered Database error {!r}, Error number- {}".format(e, e.args[0]))
+        print("-" * 10)
 
     return
 
-def dishRating(): # Finds all ratings in Feedback for given dish 
+
+def dish_rating():  # Finds all ratings in Feedback for given dish
     try:
         dish = input("Enter Dish name: ").strip()
 
-        cur.execute("SELECT Rating FROM Feedback WHERE Dish_name=%s", (dish,))        
+        cur.execute("SELECT Rating FROM Feedback WHERE Dish_name=%s", (dish,))
         rows = cur.fetchall()
         for row in rows:
             print(row)
         con.commit()
         print()
-    
+
     except MySQLError as e:
         con.rollback()
-        print('Encountered Database error {!r}, Error number- {}'.format(e, e.args[0]))
-    
-    except Exception as e:
-        con.rollback()
-        print("Failed")
-        print(">>>>>>>>>>>>>", e)
+        print("Encountered Database error {!r}, Error number- {}".format(e, e.args[0]))
+        print("-" * 10)
 
     return
 
-def avgEmpRating(): # Finds average rating for given Employee 
+
+def avg_emp_rating():  # Finds average rating for given Employee
     try:
         employee = input("Enter Employee ID: ").strip()
 
-        cur.execute("SELECT Avg(Rating) FROM Feedback WHERE Waiter_id=%s OR Chef_id=%s", (employee, employee))        
+        cur.execute(
+            "SELECT Avg(Rating) FROM Feedback WHERE Waiter_id=%s OR Chef_id=%s",
+            (employee, employee),
+        )
         rows = cur.fetchall()
         print(rows)
 
@@ -98,19 +104,16 @@ def avgEmpRating(): # Finds average rating for given Employee
 
         con.commit()
         print()
-    
+
     except MySQLError as e:
         con.rollback()
-        print('Encountered Database error {!r}, Error number- {}'.format(e, e.args[0]))
-    
-    except Exception as e:
-        con.rollback()
-        print("Failed")
-        print(">>>>>>>>>>>>>", e)
+        print("Encountered Database error {!r}, Error number- {}".format(e, e.args[0]))
+        print("-" * 10)
 
     return
 
-def avgBranchRating(): # Finds average rating for a Branch
+
+def avg_branch_rating():  # Finds average rating for a Branch
     try:
         branch = input("Enter BranchID: ").strip()
         try:
@@ -119,11 +122,14 @@ def avgBranchRating(): # Finds average rating for a Branch
             print("Please enter an integer BranchID")
             raise
 
-        cur.execute("SELECT Avg(Feedback.Rating) FROM Employee INNER JOIN Feedback ON (Employee.Employee_id = Feedback.Waiter_id OR Employee.Employee_id = Feedback.Chef_id) HAVING Employee.Branch_id=%s", (branch, ))
+        cur.execute(
+            "SELECT Avg(Feedback.Rating) FROM Employee INNER JOIN Feedback ON (Employee.Employee_id = Feedback.Waiter_id OR Employee.Employee_id = Feedback.Chef_id) HAVING Employee.Branch_id=%s",
+            (branch,),
+        )
         rows = cur.fetchall()
         print(rows)
 
-        #python implementation, to be ignored
+        # python implementation, to be ignored
         """
         rows=[]
         for row in result:
@@ -138,24 +144,21 @@ def avgBranchRating(): # Finds average rating for a Branch
 
         con.commit()
         print()
-    
+
     except MySQLError as e:
         con.rollback()
-        print('Encountered Database error {!r}, Error number- {}'.format(e, e.args[0]))
-    
-    except Exception as e:
-        con.rollback()
-        print("Failed")
-        print(">>>>>>>>>>>>>", e)
+        print("Encountered Database error {!r}, Error number- {}".format(e, e.args[0]))
+        print("-" * 10)
 
     return
 
-def dishPrice(): #Updates price of a dish
-    try:
-        dish=input("Enter Dish name: ").strip()
-        price=input("Enter New price: ").strip()
 
-        cur.execute("UPDATE Dish SET Price=%d WHERE Dish_name=%s"(price,dish))
+def dish_price():  # Updates price of a dish
+    try:
+        dish = input("Enter Dish name: ").strip()
+        price = input("Enter New price: ").strip()
+
+        cur.execute("UPDATE Dish SET Price=%d WHERE Dish_name=%s"(price, dish))
         rows = cur.fetchall()
         for row in rows:
             print(row)
@@ -164,57 +167,58 @@ def dishPrice(): #Updates price of a dish
 
     except MySQLError as e:
         con.rollback()
-        print('Encountered Database error {!r}, Error number- {}'.format(e, e.args[0]))
-    except Exception as e:
-        con.rollback()
-        print("Failed")
-        print(">>>>>>>>>>>>>", e)
+        print("Encountered Database error {!r}, Error number- {}".format(e, e.args[0]))
+        print("-" * 10)
 
     return
 
+
 # -------------------------------------------------------------------------------
 # End of Functionalities
+
 
 def dispatch(ch):
     """
     Function that maps helper functions to option entered
     """
-    if(ch == 1):
-        getFeedback()
-    elif(ch == 2):
-        employeeFeedback()
-    elif(ch == 3):
-        dishRating()
-    elif(ch == 4):
-        avgEmpRating()
-    elif(ch == 5):
-        avgBranchRating()
-    elif(ch == 6):
-        dishPrice()
+    if ch == 1:
+        get_feedback()
+    elif ch == 2:
+        employee_feedback()
+    elif ch == 3:
+        dish_rating()
+    elif ch == 4:
+        avg_emp_rating()
+    elif ch == 5:
+        avg_branch_rating()
+    elif ch == 6:
+        dish_price()
     else:
         print("Error: Invalid Option")
 
 
 # Global
-while(1):
-    tmp = sp.call('clear', shell=True)
-    
+while 1:
+    tmp = sp.call("clear", shell=True)
+
     # Can be skipped if you want to hard core username and password
     username = input("Username: ")
     password = input("Password: ")
 
     try:
         # Set db name accordingly which have been create by you
-        # Set host to the server's address if you don't want to use local SQL server 
-        con = pymysql.connect(host='localhost',
-                              user=username,
-                              password=password,
-                              db='Restaurant_Ratings',
-                              cursorclass=pymysql.cursors.DictCursor,
-                              port=5005)
-        tmp = sp.call('clear', shell=True)
+        # Set host to the server's address if you don't want to use local SQL server
+        con = pymysql.connect(
+            host="localhost",
+            user=username,
+            password=password,
+            db="Restaurant_Ratings",
+            cursorclass=pymysql.cursors.DictCursor,
+            port=5005,
+        )
+        tmp = sp.call("clear", shell=True)
 
-        if(con.open):
+        if con.open:
             print("Connected")
         else:
             print("Failed to connect")
@@ -222,20 +226,29 @@ while(1):
         tmp = input("Enter any key to CONTINUE>")
 
         with con.cursor() as cur:
-            while(1):
-                tmp = sp.call('clear', shell=True)
-                # Here taking example of Employee Mini-world
-                print("Enter a number to select corresponding option:-")
-                print("0 - Logout")
-                print("1 - Get a specific Feedback") #Select query
-                print("2 - Get Feedback for an employee") #Select query
-                print("3 - Get all Ratings for a dish") #Project query
-                print("4 - Get Average rating for an Employee") #Aggregate query
-                print("5 - Get Average rating for a Branch") #Analysis - Join and Aggregate
-                print("6 - Update the price of a Dish") #Update
+            while 1:
+                tmp = sp.call("clear", shell=True)
 
-                ch = int(input("Enter choice> "))
-                tmp = sp.call('clear', shell=True)
+                print("Enter a number to select corresponding option:-")
+                # Queries
+                print("0 - Logout")
+                print("1 - Get a specific Feedback")  # Select query
+                print("2 - Get Feedback for an employee")  # Select query
+                print("3 - Get all Ratings for a dish")  # Project query
+                print("4 - Get Average rating for an Employee")  # Aggregate query
+                print(
+                    "5 - Get Average rating for a Branch"
+                )  # Analysis - Join and Aggregate
+                # Updates
+                print("6 - Update the price of a Dish")  # Update
+
+                try:
+                    ch = int(input("Enter choice> "))
+                except TypeError:
+                    print("\nPlease enter an integer\n")
+                    raise
+
+                tmp = sp.call("clear", shell=True)
                 if ch == 0:
                     break
                 else:
@@ -243,16 +256,16 @@ while(1):
                     tmp = input("Enter any key to CONTINUE>")
 
     except:
-        tmp = sp.call('clear', shell=True)
-        print("Connection Refused: Either username or password is incorrect or user doesn't have access to database")
+        tmp = sp.call("clear", shell=True)
+        print(
+            "Connection Refused: Either username or password is incorrect or user doesn't have access to database"
+        )
         tmp = input("Enter any key to CONTINUE>")
 
 
+# ---------------------------------------------------------------------------------
+# redundant features kept for an unassumed requirement later (better safe than sorry :) )
 """
----------------------------------------------------------------------------------
-#redundant features kept for an unassumed requirement later (better safe than sorry :) )
-
-
 def hireAnEmployee():
     # This is a sample function implemented for the refrence.
     # This example is related to the Employee Database.
